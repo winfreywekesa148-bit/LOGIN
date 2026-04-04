241d47daeb1b0975dd857097140729dc1973037b
Winfrey Ward Wekesa <winfreywekesa148@gmail.com>
2026-04-04 19:39:10 +0300

    Initialize login tracking

--- index.js
+++ index.js
@@ -14,6 +14,16 @@ function createLoginTracker(userInfo) {
     "username" = "user 1";
     "password" = "password";
 
+    function attemptCount() {
+        let attempts = 3;if (attempts > 0 && attempts <= 3) {
+    console.log(`You have ${attempts} attempts left.`);
+    attempts--;
+         } else {
+    console.log(" Account locked.No more attempts left."); 
+    }
+
+   }
+
 }
 
 // 
