8f878987196022dbcb8e8d4c6fba8c5d19dfc8cc
Winfrey Ward Wekesa <winfreywekesa148@gmail.com>
2026-04-04 19:16:01 +0300

    creation of the user information

--- index.js
+++ index.js
@@ -9,3 +9,18 @@ if (attempts > 0 && attempts <= 3) {
     console.log(" Account locked.No more attempts left.");
 }
 
+// user info
+function createLoginTracker(userInfo) {
+    username = userInfo.username;
+    password = userInfo.password;
+
+    return function createLoginTracker() {
+        if (attempts > 0 && attempts <= 3) {
+    console.log(`You have ${attempts} attempts left.`);
+    attempts--;
+        } else {
+    console.log(" Account locked.No more attempts left.");
+    
+    }  
+}
+}
