b3e69f8c9c314b10fd62e0053988ff687306c83f
Winfrey Ward Wekesa <winfreywekesa148@gmail.com>
2026-04-05 13:51:44 +0300

    debug of the website

--- index.js
+++ index.js
@@ -1,21 +1,10 @@
-// 3 attempts of login 
-
-let attempts = 3;
-
-if (attempts > 0 && attempts <= 3) {
-    console.log(`You have ${attempts} attempts left.`);
-    attempts--;
-} else {
-    console.log(" Account locked.No more attempts left.");
-}
-
 // user info
 function createLoginTracker(userInfo) {
-    "username" = "user 1";
-    "password" = "password";
+    let attempts = 3;
 
-    function attemptCount() {
-        let attempts = 3;if (attempts > 0 && attempts <= 3) {
+    function attemptCount(attempts) {
+        
+        if (attempts > 0 && attempts <= 3) {
     console.log(`You have ${attempts} attempts left.`);
     attempts--;
          } else {
@@ -24,6 +13,32 @@ function createLoginTracker(userInfo) {
 
    }
 
+   function passwordAttempt( password) {
+    if (attempts <= 0) {
+        console.log("Account locked. No more attempts left.");
+    }
+
+    if (password === userInfo.password) {
+        console.log("Login successfull");
+        attempts = 3; //reset
+    } else {
+        attemptCount(attempts);
+    }
+   }
+   return {
+    passwordAttempt
+   };
+
 }
 
-// 
+const userInfo = {
+    username: "user 1",
+    password: "password123"
+}
+
+const tracker = createLoginTracker(userInfo);
+
+tracker.passwordAttempt("wrongpassword");
+tracker.passwordAttempt("password123");
+
+console.log(createLoginTracker(userInfo));
