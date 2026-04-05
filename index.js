359979bfcab182e672e5a3dcf27c165c54a43fad
Winfrey Ward Wekesa <winfreywekesa148@gmail.com>
2026-04-05 14:42:33 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,44 +1,5 @@
-// user info
-function createLoginTracker(userInfo) {
-    let attempts = 3;
-
-    function attemptCount(attempts) {
-        
-        if (attempts > 0 && attempts <= 3) {
-    console.log(`You have ${attempts} attempts left.`);
-    attempts--;
-         } else {
-    console.log(" Account locked.No more attempts left."); 
-    }
-
-   }
-
-   function passwordAttempt( password) {
-    if (attempts <= 0) {
-        console.log("Account locked. No more attempts left.");
-    }
-
-    if (password === userInfo.password) {
-        console.log("Login successfull");
-        attempts = 3; //reset
-    } else {
-        attemptCount(attempts);
-    }
-   }
-   return {
-    passwordAttempt
-   };
-
+if (require.main === module) {
+  require('./lib/cli.js')(process)
+} else {
+  throw new Error('The programmatic API was removed in npm v8.0.0')
 }
-
-const userInfo = {
-    username: "user 1",
-    password: "password123"
-}
-
-const tracker = createLoginTracker(userInfo);
-
-tracker.passwordAttempt("wrongpassword");
-tracker.passwordAttempt("password123");
-
-console.log(createLoginTracker(userInfo));
